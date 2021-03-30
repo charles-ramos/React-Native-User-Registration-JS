8edd0e78f218c4801a1f857ff2a7f0552056641a
charles-ramos <charles@back4app.com>
2021-03-30 19:12:01 -0300

    first commit

---
+++ constants/Keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    applicationId: 'lOnJ1j6c5OHzz7RFmlXATbEqGvKA3BuT9KXGyWdO',
+    javascriptKey: 'n1u4b9uVkjUKq9QlZcKWfiHrf45zY1PsegXOaoT7',
+    serverURL: 'https://parseapi.back4app.com/'
+   }
